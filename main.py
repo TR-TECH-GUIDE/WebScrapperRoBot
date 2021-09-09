8bef97b8ca20a087e4812dcada348d4963f86f38
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-09 21:18:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from bs4 import BeautifulSoup
 import requests
 from telegraph.api import Telegraph
+from script import script
 
 SLBotsOfficial = Client(
     "WebScrapperBot",
@@ -23,13 +24,25 @@ telegraph = Telegraph(
 @SLBotsOfficial.on_message(filters.command(["start"]))
 async def start(_, message: Message):
     await message.reply_text("I can Scrape a website within seconds. Send me A website link and Get Your results now Want To Know More About Web Scraping? Read Help")  # Edit Your Start string here
-
+                        reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("HELP", callback_data="help_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/SLBotsOfficial")
+                    ]
+                ]
+            )
+        
 @SLBotsOfficial.on_message(filters.command(["help"]))
 async def help(_, message: Message):
     await message.reply_text("Web scraping, web harvesting, or web data extraction is data scraping used for extracting data from websites. The web scraping software may directly access the World Wide Web using the Hypertext Transfer Protocol or a web browser.")
 
 @SLBotsOfficial.on_message(filters.command(["about"]))
-async def about(client, message):
+async def about(_, message):
     await message.reply_text(
             text=script.ABOUT_TEXT.format(message.from_user.mention),
             disable_web_page_preview=True,
