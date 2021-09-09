e44d62bb9165113bd4ba0f339ea081435793b01b
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-09 21:08:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ async def help(_, message: Message):
 
 @SLBotsOfficial.on_message(filters.command(["about"]))
 async def about(client, message):
-    try:
-        await message.reply_text(
+    await message.reply_text(
             text=script.ABOUT_TEXT.format(message.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -45,11 +44,7 @@ async def about(client, message):
                             "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/SLBotsOfficial")
                     ]
                 ]
-            ),
-            reply_to_message_id=message.message_id
-        )
-    except:
-        pass
+            )
     
 ABOUT_TEXT = """
 ● BOT: Web Scrapper Bot 
