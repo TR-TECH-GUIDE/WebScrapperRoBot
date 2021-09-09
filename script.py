8f7b69c1240813ec983d6e7ea567c800071d494f
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-09 21:14:34 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+class script(object):
+  
+  ABOUT_TEXT = """
+● BOT: Web Scrapper Bot 
+● AUTHOR : Tharuk (https://t.me/TharukRenuja) 
+● SERVER : Heroku 
+● LIBRARY : Pyrogram 
+● LANGUAGE : Python 3.9
+
+© **Powered By** : 
+**@SLBotsOfficial** 👑
+"""
