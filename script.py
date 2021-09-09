d1594ca384c72f01103d6bddc281b283711eba1a
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-09 21:53:17 +0530

    Update script.py

--- script.py
+++ script.py
@@ -10,3 +10,32 @@ class script(object):
 © **Powered By** : 
 **@SLBotsOfficial** 👑
 """
+  START_MSG = """
+Hi I'm WebScrapper,
+I can Scrape a website within seconds. 
+Send me A website link and Get Your results now
+Want To Know More About Web Scraping? Read Help
+
+© **Powered By** : 
+**@SLBotsOfficial** 👑
+"""
+  
+  HELP_MSG = """
+What is Web Scraping ? 
+Web scraping, web harvesting, or web data extraction is data scraping used for extracting data from websites. The web scraping software may directly access the World Wide Web using the Hypertext Transfer Protocol or a web browser.
+
+Is web scraping Legal?
+Web scraping itself is not illegal. As a matter of fact, web scraping – or web crawling, were historically associated with well-known search engines like Google or Bing. These search engines crawl sites and index the web. ... A great example when web scraping can be illegal is when you try to scrape nonpublic data.
+
+Why web scraping is Done?
+Web scraping is used in a variety of digital businesses that rely on data harvesting. Legitimate use cases include: Search engine bots crawling a site, analyzing its content and then ranking it. ... Market research companies using scrapers to pull data from forums and social media (e.g., for sentiment analysis).
+
+Where can I use web scraping?
+Lead Generation for Marketing. A web scraping software can be used to generate leads for marketing,Price Comparison & Competition Monitoring,E-Commerce,Real Estate,Data Analysis,Academic Research,Training and Testing Data for Machine Learning Projects,,Sports Betting Odds Analysis.
+
+Is there any Limitations?
+Learning curve, Even the easiest scraping tool takes time to master,The structure of websites change frequently,Scraped data is arranged according to the structure of the website,It is not easy to handle complex websites,To extract data on a large scale is way harder,A web scraping tool is not omnipotent
+
+© **Powered By** : 
+**@SLBotsOfficial** 👑
+"""
