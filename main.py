54625c447a09a5be85eea0958b1af8f7f45f3a05
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-10 10:44:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,6 @@ async def start(client, message):
                 [
                     [
                         InlineKeyboardButton(
-                            "❔ Help", url="http://t.me/WebScrappeBot?start=/help")
-                    ],
-                    [
-                        InlineKeyboardButton(
                             "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/SLBotsOfficial")
                     ]
                 ]
@@ -51,10 +47,6 @@ async def help(client, message):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [
-                        InlineKeyboardButton(
-                            "♻About", url="http://t.me/WebScrappeBot?start=/about")
-                    ]
                     [
                         InlineKeyboardButton(
                             "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/SLBotsOfficial")
