8a389ae57b9e2cb875bc3b33e66ecb5f48c86cf8
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-10 08:53:26 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ class script(object):
   
   ABOUT_TEXT = """
 ● BOT: Web Scrapper Bot 
-● AUTHOR : Tharuk (https://t.me/TharukRenuja) 
+● AUTHOR : [Tharuk] (https://t.me/TharukRenuja) 
 ● SERVER : Heroku 
 ● LIBRARY : Pyrogram 
 ● LANGUAGE : Python 3.9
